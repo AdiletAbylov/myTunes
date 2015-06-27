ad0c4b4935a66e1485e3921c381d036820a6a6d2
Adilet Abylov <a.abylov@gmail.com>
2015-06-27 21:15:27 +0600

    Sanitize filename before rename

--- myTunes.rb
+++ myTunes.rb
@@ -12,11 +12,28 @@ end
 def process_file(path)
 	if File.extname(path) == ".mp3"
 		Mp3Info.open(path) do |info|
-			new_file_name = [info.tag.artist, info.tag.album, "%02d" % info.tag.tracknum,info.tag.title ].join(" - ")
+			new_file_name = [info.tag.artist, info.tag.album, "%02d" % info.tag.tracknum?:0, info.tag.title ].join(" - ")
+			new_file_name = sanitize_filename(new_file_name)
 			puts new_file_name
 			File.rename(path, new_file_name + ".mp3")
 		end
 	end
+end
+
+def sanitize_filename(filename)
+  # Split the name when finding a period which is preceded by some
+  # character, and is followed by some character other than a period,
+  # if there is no following period that is followed by something
+  # other than a period (yeah, confusing, I know)
+  fn = filename.split /(?<=.)\.(?=[^.])(?!.*\.[^.])/m
+
+  # We now have one or two parts (depending on whether we could find
+  # a suitable period). For each of these parts, replace any unwanted
+  # sequence of characters with an underscore
+  fn.map! { |s| s.gsub /[^a-z0-9\-]+/i, '_' }
+
+  # Finally, join the parts with a period and return the result
+  return fn.join '.'
 end
 
 options = {}
